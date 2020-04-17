f6cbad876ec41a91d4e0cf2b86a13c8fd78ec516
Thomas <thomasbruggink@hotmail.com>
2020-04-17 19:26:56 +0900

    Allow filtering in config

--- Finder.cs
+++ Finder.cs
@@ -6,7 +6,6 @@ using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using ContainerKiller.Exceptions;
 using ContainerKiller.Models;
 using Mono.Unix;
--- KillerConfig.cs
+++ KillerConfig.cs
@@ -3,6 +3,7 @@ namespace ContainerKiller
     public class KillerConfig
     {
         public string ImageName { get; set; }
+        public string ContainerName { get; set; }
         public string ExpectedNetwork { get; set; }
     }
 }
